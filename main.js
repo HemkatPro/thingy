cdafa7dd96a83e91f916379b6532431e5f520fe6
HemkatPro <89548921+HemkatPro@users.noreply.github.com>
2021-09-17 16:24:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+canvas {
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 5;
+    ctx.arc(250, 210, 40, 0, 2 * MATH.PI);
+    ctx.stroke;
+    ctx.beginPath();
+    ctx.strokestyle = "green";
+    ctx.linewidth = 5;
+    ctx.arc(300, 210, 40, 0, 2 * MATH.PI);
+    ctx.stroke;
+    ctx.beginPath();
+    ctx.strokestyle = "black";
+    ctx.linewidth = 5;
+    ctx.arc(350, 210, 40, 0, 2 * MATH.PI);
+    ctx.stroke;
+    ctx.beginPath();
+    ctx.strokestyle = "yellow";
+    ctx.linewidth = 5;
+    ctx.arc(275, 170, 40, 0, 2 * MATH.PI);
+    ctx.stroke;
+    ctx.beginPath();
+    ctx.strokestyle = "red";
+    ctx.linewidth = 5;
+    ctx.arc(325, 170, 40, 0, 2 * MATH.PI);
+    ctx.stroke;
+
+}
